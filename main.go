bb707474588d8374161ba538c960a6f315b73687
rifki zulkarnain <rifkizlkrnn@gmail.com>
2024-11-12 11:01:33 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Sajikan file statis untuk CSS
+	r.Static("/static", "./static")
+
+	// Setel template HTML
+	r.LoadHTMLGlob("templates/*.html")
+
+	// Routing untuk halaman Utama
+	r.GET("", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "layout.html", gin.H{
+			"content": "rooms.html",
+		})
+	})
+
+	r.Run(":8080")
+}
